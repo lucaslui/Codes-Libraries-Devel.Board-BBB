81fe09280a644556330cdfab7b99df5aceb86143
Lucas Lui Motta <lucasluimotta@gmail.com>
2015-05-04 02:18:13 -0300

    Update GPIO.cpp

--- Created Libraries - BBB/GPIO.cpp
+++ Created Libraries - BBB/GPIO.cpp
@@ -2,7 +2,7 @@
  * Arquivo: GPIO.cpp
  * Descrição do arquivo: Arquivo do código da classe GPIO, na qual faz a criação, acesso e
  * 			 configuração dos pinos de próposito geral da beaglebone.
- * Criado em: 02/05/2015
+ * Criado em: 05/01/2015
  * Autor: Lucas Lui Motta.
  */
 
